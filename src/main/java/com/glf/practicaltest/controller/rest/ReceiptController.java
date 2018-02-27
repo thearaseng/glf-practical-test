b75bf504520c354b2453f9e44fc21027d2621030
Theara Seng <seng.theara.kh@gmail.com>
2018-02-27 10:52:22 +0700

    remove import

--- src/main/java/com/glf/practicaltest/controller/rest/ReceiptController.java
+++ src/main/java/com/glf/practicaltest/controller/rest/ReceiptController.java
@@ -6,12 +6,9 @@ import com.glf.practicaltest.service.ReceiptService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 /**
  * @author Theara Seng
