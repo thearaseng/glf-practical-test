7a1a71eca48d9926679968d27c8e18eeeedd6095
Theara Seng <seng.theara.kh@gmail.com>
2018-02-27 17:11:05 +0700

    fix calculate total amount

--- src/main/java/com/glf/practicaltest/service/impl/OrderServiceImpl.java
+++ src/main/java/com/glf/practicaltest/service/impl/OrderServiceImpl.java
@@ -4,6 +4,7 @@ import com.glf.practicaltest.model.Order;
 import com.glf.practicaltest.model.command.OrderCommand;
 import com.glf.practicaltest.model.converter.OrderCommandToOrder;
 import com.glf.practicaltest.repository.OrderRepository;
+import com.glf.practicaltest.service.OperationService;
 import com.glf.practicaltest.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,20 +18,27 @@ import org.springframework.stereotype.Service;
 public class OrderServiceImpl implements OrderService{
 
     private OrderRepository orderRepository;
+    private OperationService operationService;
     private OrderCommandToOrder orderCommandToOrder;
 
     @Autowired
-    public OrderServiceImpl(OrderRepository orderRepository, OrderCommandToOrder orderCommandToOrder) {
+    public OrderServiceImpl(OrderRepository orderRepository, OperationService operationService, OrderCommandToOrder orderCommandToOrder) {
         this.orderRepository = orderRepository;
+        this.operationService = operationService;
         this.orderCommandToOrder = orderCommandToOrder;
     }
 
     @Override
     public Order save(OrderCommand orderCommand) {
 
-        if (orderCommand.getAmount() > 2_000D) {
+        boolean isAmountOver2000 = orderCommand.getAmount() > 2_000D;
+
+        if (isAmountOver2000) {
             orderCommand.setAmount(orderCommand.getAmount() * 0.95);
         }
+
+        if (orderCommand.getOperationId() != 1 || isAmountOver2000)
+            orderCommand.setTotalAmount(orderCommand.getTotalAmount() + operationService.findById(orderCommand.getOperationId()).getPrice());
 
         return orderRepository.save(orderCommandToOrder.convert(orderCommand));
     }
