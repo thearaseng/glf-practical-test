fc5e7ed3124920065fea9efac4f4c6add01890e3
Theara Seng <seng.theara.kh@gmail.com>
2018-02-27 10:17:18 +0700

    add findAll() to ReceiptService

--- src/main/java/com/glf/practicaltest/service/ReceiptService.java
+++ src/main/java/com/glf/practicaltest/service/ReceiptService.java
@@ -1,9 +1,16 @@
 package com.glf.practicaltest.service;
 
+import com.glf.practicaltest.model.Receipt;
+
+import java.util.List;
+
 /**
  * @author Theara Seng
  * Created on 27 Feb, 2018
  */
 
 public interface ReceiptService {
+
+    List<Receipt> findAll();
+
 }
--- src/main/java/com/glf/practicaltest/service/impl/ReceiptServiceImpl.java
+++ src/main/java/com/glf/practicaltest/service/impl/ReceiptServiceImpl.java
@@ -1,12 +1,35 @@
 package com.glf.practicaltest.service.impl;
 
+import com.glf.practicaltest.model.Receipt;
+import com.glf.practicaltest.repository.ReceiptRepository;
 import com.glf.practicaltest.service.ReceiptService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Theara Seng
  * Created on 27 Feb, 2018
  */
 
+@Service
 public class ReceiptServiceImpl implements ReceiptService {
+
+    private ReceiptRepository receiptRepository;
+
+    @Autowired
+    public ReceiptServiceImpl(ReceiptRepository receiptRepository) {
+        this.receiptRepository = receiptRepository;
+    }
+
+    @Override
+    public List<Receipt> findAll() {
+        List<Receipt> receipts = new ArrayList<>();
+        receiptRepository.findAll().forEach(receipt -> receipts.add(receipt));
+
+        return receipts;
+    }
 
 }
