dcc2e5e025ebc0ecd57a7d0868919304e811dd5d
Theara Seng <seng.theara.kh@gmail.com>
2018-02-27 16:15:33 +0700

    create OrderCommand and OrderCommandToOrder

---
+++ src/main/java/com/glf/practicaltest/model/command/OrderCommand.java
@@ -0,0 +1,47 @@
+package com.glf.practicaltest.model.command;
+
+/**
+ * @author Theara Seng
+ * Created on 27 Feb, 2018
+ */
+
+public class OrderCommand {
+
+    private Long id;
+    private Long operationId;
+    private String description;
+    private Double amount;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getOperationId() {
+        return operationId;
+    }
+
+    public void setOperationId(Long operationId) {
+        this.operationId = operationId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+}
---
+++ src/main/java/com/glf/practicaltest/model/converter/OrderCommandToOrder.java
@@ -0,0 +1,38 @@
+package com.glf.practicaltest.model.converter;
+
+import com.glf.practicaltest.model.Order;
+import com.glf.practicaltest.model.command.OrderCommand;
+import com.glf.practicaltest.service.OperationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Theara Seng
+ * Created on 27 Feb, 2018
+ */
+
+@Component
+public class OrderCommandToOrder implements Converter<OrderCommand, Order> {
+
+    private OperationService operationService;
+
+    @Autowired
+    public OrderCommandToOrder(OperationService operationService) {
+        this.operationService = operationService;
+    }
+
+    @Override
+    public Order convert(OrderCommand orderCommand) {
+        if (orderCommand == null)
+            return null;
+
+        final Order order = new Order();
+        order.setId(orderCommand.getId());
+        order.setAmount(orderCommand.getAmount());
+        order.setDescription(orderCommand.getDescription());
+        order.setOperation(operationService.findById(orderCommand.getOperationId()));
+
+        return order;
+    }
+}
