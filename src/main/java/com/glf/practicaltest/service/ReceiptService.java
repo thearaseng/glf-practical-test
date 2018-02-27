d37378f63980866dbec657001a4fd1a73ffbc035
Theara Seng <seng.theara.kh@gmail.com>
2018-02-27 10:09:46 +0700

    create ReceiptService and ReceiptServiceImpl

---
+++ src/main/java/com/glf/practicaltest/service/ReceiptService.java
@@ -0,0 +1,9 @@
+package com.glf.practicaltest.service;
+
+/**
+ * @author Theara Seng
+ * Created on 27 Feb, 2018
+ */
+
+public interface ReceiptService {
+}
---
+++ src/main/java/com/glf/practicaltest/service/impl/ReceiptServiceImpl.java
@@ -0,0 +1,12 @@
+package com.glf.practicaltest.service.impl;
+
+import com.glf.practicaltest.service.ReceiptService;
+
+/**
+ * @author Theara Seng
+ * Created on 27 Feb, 2018
+ */
+
+public class ReceiptServiceImpl implements ReceiptService {
+
+}
